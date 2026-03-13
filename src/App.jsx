d852328f36bb1cb165416c4786cd85d31b52b82d
minh-git-23 <himinhgit@gmail.com>
2026-03-14 01:16:14 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,29 +3,32 @@ import "./App.css";
 import ProductList from "./components/ProductList";
 
 function App() {
+
   const [showProductList, setShowProductList] = useState(false);
 
   return (
-    <div>
+    <div className="app-container">
+
       {showProductList ? (
+
         <ProductList />
+
       ) : (
+
         <div className="landing-page">
-          <div className="landing-content">
-            <h1>Welcome to Paradise Nursery</h1>
-            <p>
-              Discover a wide variety of beautiful houseplants to brighten your
-              home and improve your lifestyle.
-            </p>
-            <button
-              className="get-started-btn"
-              onClick={() => setShowProductList(true)}
-            >
-              Get Started
-            </button>
-          </div>
+
+          <h1>Welcome to Paradise Nursery</h1>
+
+          <button
+            onClick={() => setShowProductList(true)}
+          >
+            Get Started
+          </button>
+
         </div>
+
       )}
+
     </div>
   );
 }
